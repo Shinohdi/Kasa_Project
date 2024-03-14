9641259505f38e47af05515d59ab1cbce35de0e9
Mahdi <Mahdi__Amri@hotmail.com>
2024-03-14 14:49:35 +0100

    Début page d'accueil
    Mise en place de la bannière de description de la page d'accueil

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
+import '../../styles/Home/Home.css';
+import background from '../../images/e270fc8fc902a1eb738458e7b29c1899.jpg';
+
 function Home() {
-  return (
-    <div>
-      <h1> Page d'accueil 🏠</h1>
-    </div>
-  )
+    return (
+        <div className="description">
+            <div className="description__text">
+                <h1>Chez vous, partout et ailleurs</h1>
+            </div>
+            <div className="description__mask">
+                <img src={background} alt="Description Background" />
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
